e2610eef10450da818eb700986869002c5c72c55
Yee Hung <19034609@myrp.edy.sg>
2021-05-17 16:16:16 +0800

    commit

--- app/src/main/java/sg/edu/rp/c347/id19045104/nationaldayparadethemesongcompilation/ShowActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19045104/nationaldayparadethemesongcompilation/ShowActivity.java
@@ -3,13 +3,18 @@ package sg.edu.rp.c347.id19045104.nationaldayparadethemesongcompilation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ShowActivity extends AppCompatActivity {
 
     Button btn5stars;
     ListView lv;
+    ArrayList<Song> al;
+    ArrayAdapter<Song> aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +24,20 @@ public class ShowActivity extends AppCompatActivity {
         btn5stars = findViewById(R.id.btn5stars);
         lv = findViewById(R.id.lv);
 
+        al = new ArrayList<Song>();
 
+        DBHelper dbh = new DBHelper(ShowActivity.this);
+        al.clear();
+        al.addAll(dbh.getAllNotes(""));
+        dbh.close();
 
+        String txt = "";
+        for (int i = 0; i< al.size(); i++){
+            Note tmp = al.get(i);
+            txt += "ID:" + tmp.getId() + ", " + tmp.getNoteContent() + "\n";
+        }
+        tvDBContent.setText(txt);
+        aa.notifyDataSetChanged();
 
 
 

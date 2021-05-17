5051955b9cd118421e5b63634edb28b55180d7a3
19045104 <19045104@myrp.edu.sg>
2021-05-17 18:26:57 +0800

    Update

--- app/src/main/java/sg/edu/rp/c347/id19045104/nationaldayparadethemesongcompilation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19045104/nationaldayparadethemesongcompilation/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -16,9 +17,10 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     //this is a test line only
     EditText etSong, etSinger, etYear;
-    RadioGroup rgStar;
+    RadioGroup rgStars;
     Button btnInsert, btnShow;
     ArrayList<Song> al;
+    RadioButton rgstar;
 
 
     @Override
@@ -29,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         etSong = findViewById(R.id.etSong);
         etSinger = findViewById(R.id.etSinger);
         etYear = findViewById(R.id.etYear);
-        rgStar = findViewById(R.id.rgStar);
+        rgStars = findViewById(R.id.rgStar);
         btnInsert = findViewById(R.id.btnInsert);
         btnShow = findViewById(R.id.btnShow);
 
@@ -40,9 +42,12 @@ public class MainActivity extends AppCompatActivity {
                 String song = etSong.getText().toString();
                 String singer = etSinger.getText().toString();
                 int year = Integer.valueOf(etYear.getText().toString());
-                int stars = rgStar.getCheckedRadioButtonId();
+                rgstar = findViewById(rgStars.getCheckedRadioButtonId());
+                int star = Integer.valueOf(rgstar.getText().toString());
+
+
                 DBHelper dbh = new DBHelper(MainActivity.this);
-                long inserted_id = dbh.insertSong(song,singer,year,stars);
+                long inserted_id = dbh.insertSong(song,singer,year,star);
                 if (inserted_id != -1){
                     Toast.makeText(MainActivity.this, "Insert Successfull", Toast.LENGTH_SHORT).show();
                 }
